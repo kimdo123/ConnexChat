94196aaf0b89d8f3fc6f67ef92eafc8f7133a627
김도현 <01091493420a@gmail.com>
2025-11-18 21:25:27 +0900

    11/18

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:connex_chat/view/login.dart';
+import 'package:connex_chat/view/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: LoginScreen(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'lex'),
-    )
+      theme: ThemeData(
+        fontFamily: 'lex',
+        scaffoldBackgroundColor: Colors.white,
+      ),
+    ),
   );
 }
